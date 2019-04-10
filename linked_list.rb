7d6ea8c85197e54abb0f08e756004ad86765c342
Aaron Collins <aa.collins.95@gmail.com>
2019-04-10 10:06:25 -0400

    LL's can prepend

--- linked_list.rb
+++ linked_list.rb
@@ -7,6 +7,7 @@ class LinkedList
   end
 
   def disp
+    #quickdirty method to display the LL
     curr_node = @head
     while curr_node != @tail
       puts curr_node.data
@@ -15,10 +16,20 @@ class LinkedList
   end
 
   def append(data)
+    #adds a data to the end of the LL
     if data.is_a? @type
       @tail.next_node = Node.new
       @tail.data = data
       @tail = @tail.next_node
+    else
+      puts "need data of type #{@type}"
+    end
+  end
+
+  def prepend(data)
+    if data.is_a? @type
+      new = Node.new(data,@head)
+      @head = new
     else
       puts "need data of type #{@type}"
     end
@@ -39,4 +50,5 @@ end
 
 list = LinkedList.new(0)
 (1..10).each do |i| list.append(i) end
+(11..20).each do |i| list.prepend(i) end
 list.disp
